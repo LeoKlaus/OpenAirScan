c3518e4532d17bd7bec4a91f6bb00ee73c6f99de
Leo Wehrfritz <leocommander.cs@gmail.com>
2022-07-14 09:45:34 +0200

    Initial Commit

---
+++ AirScanTest/AirScanTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirScanTestApp.swift
+//  AirScanTest
+//
+//  Created by Leo Wehrfritz on 14.07.22.
+//
+
+import SwiftUI
+
+@main
+struct AirScanTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
