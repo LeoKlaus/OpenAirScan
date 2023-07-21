e0b211d83e794f7d7884aa5ea9acb45751f01523
Leo Wehrfritz <leocommander.cs@gmail.com>
2023-07-21 14:16:15 +0200

    Added GUI support for logging

---
+++ OpenAirScan/Subviews/LogDisplay.swift
@@ -0,0 +1,18 @@
+//
+//  LogDisplay.swift
+//  OpenAirScan
+//
+//  Created by Leo Wehrfritz on 21.07.23.
+//
+
+import SwiftUI
+
+struct LogDisplay: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LogDisplay()
+}
